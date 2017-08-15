529f3b9b098765f22ffb156316cbbb5b959bf6ca
Tiago <tiago@makesystem.com.br>
2017-08-15 18:13:14 -0300

    Finalizado os testes das Rns

--- src/java/br/com/api/io/services/Teste.java
+++ src/java/br/com/api/io/services/Teste.java
@@ -1,13 +1,6 @@
 package br.com.api.io.services;
 
-import br.com.entitys.User;
-import br.com.filters.EntityFilter;
-import br.com.filters.UserFilter;
-import br.com.rn.UserRn;
 import java.io.Serializable;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.ejb.Stateless;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
@@ -31,15 +24,6 @@ public class Teste implements Serializable {
     @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
     @Path("/teste")
     public String teste() {
-        UserRn rn = new UserRn();
-        UserFilter filter = new UserFilter();
-        filter.setDirection(EntityFilter.Direction.ASC);
-        try {
-            List<User> list = rn.list(filter);
-            System.out.println(list.size());
-        } catch (Throwable ex) {
-            Logger.getLogger(Teste.class.getName()).log(Level.SEVERE, null, ex);
-        }
         return "true";
     }
 
